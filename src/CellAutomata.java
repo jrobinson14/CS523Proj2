bd309e4ca897bf40c265aa5e028b6dce49a88eab
Jonathan Robinson <jrobinson14@unm.edu>
2020-03-15 12:20:54 -0600

    add check neighbors method

--- src/CellAutomata.java
+++ src/CellAutomata.java
@@ -4,12 +4,15 @@ public class CellAutomata implements Runnable {
     private int neighborhood;
     public int day; //what day it is in the sim
     private Cell[][] cellArray; //stores all cells
+    private String simType; //governs if sim is discrete or using probabilities
 
 
-    public CellAutomata(int size, int neighborhood){
+    public CellAutomata(int size, int neighborhood, String type){
         this.size = size;
         this.neighborhood = neighborhood;
         this.cellArray = new Cell[size][size];
+        this.simType = type;
+        System.out.println("Running sim in mode: " + simType);
         createAutomata();
 
     }
@@ -44,5 +47,78 @@ public class CellAutomata implements Runnable {
     //just for GUI testing
     public void infect(int x, int y){
         cellArray[x][y].cellState = States.Infected;
+    }
+
+    public States checkNeighbors(int i, int j){
+        int sickNeighbors = 0;
+        States newState = States.Susceptible;
+        //check top left
+        try{
+            if(cellArray[i-1][j-1].cellState == States.Infected)
+                sickNeighbors++;
+        } catch(IndexOutOfBoundsException e){
+            System.out.printf("Cell %d has no top left neighbor here\n", cellArray[i][j].ID);
+            sickNeighbors++; //this is just to balance cells with fewer neighbors (corner neighbors cannot be infected  in discrete sim without this!)
+        }
+        //check top center
+        try{
+            if(cellArray[i-1][j].cellState == States.Infected)
+                sickNeighbors++;
+        } catch(IndexOutOfBoundsException e){
+            System.out.printf("Cell %d has no top center neighbor here\n", cellArray[i][j].ID);
+        }
+        //check top right
+        try{
+            if(cellArray[i-1][j+1].cellState == States.Infected)
+                sickNeighbors++;
+        } catch(IndexOutOfBoundsException e){
+            System.out.printf("Cell %d has no top right neighbor here\n", cellArray[i][j].ID);
+        }
+        //check center left
+        try{
+            if(cellArray[i][j-1].cellState == States.Infected)
+                sickNeighbors++;
+        } catch(IndexOutOfBoundsException e){
+            System.out.printf("Cell %d has no center left neighbor here\n", cellArray[i][j].ID);
+        }
+        //check center right
+        try{
+            if(cellArray[i][j+1].cellState == States.Infected)
+                sickNeighbors++;
+        } catch(IndexOutOfBoundsException e){
+            System.out.printf("Cell %d has no  center right neighbor here\n", cellArray[i][j].ID);
+        }
+        //check bottom left
+        try{
+            if(cellArray[i+1][j-1].cellState == States.Infected)
+                sickNeighbors++;
+        } catch(IndexOutOfBoundsException e){
+            System.out.printf("Cell %d has no  bottome left neighbor here\n", cellArray[i][j].ID);
+        }
+        //check bottome center
+        try{
+            if(cellArray[i+1][j].cellState == States.Infected)
+                sickNeighbors++;
+        } catch(IndexOutOfBoundsException e){
+            System.out.printf("Cell %d has no  bottom center neighbor here\n", cellArray[i][j].ID);
+        }
+        //check bottome right
+        try{
+            if(cellArray[i+1][j+1].cellState == States.Infected)
+                sickNeighbors++;
+        } catch(IndexOutOfBoundsException e){
+            System.out.printf("Cell %d has no bottome right neighbor here\n", cellArray[i][j].ID);
+        }
+
+        System.out.printf("Cell %d has finished checking, found %d infections\n", cellArray[i][j].ID, sickNeighbors);
+
+        if(simType.equals("Discrete")){
+            if(sickNeighbors >= 4) {
+                System.out.printf("Cell %d is now infected\n", cellArray[i][j].ID);
+                newState = States.Infected;
+            } else
+                System.out.printf("Cell %d is ok!\n", cellArray[i][j].ID);
+        }
+        return newState;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        CellAutomata testAut = new CellAutomata(100, 1);
-        Display board = new Display(testAut,100);
-        Thread.sleep(500);
+        //testing code
+        CellAutomata testAut = new CellAutomata(3, 3, "Discrete");
+        Display board = new Display(testAut,3);
+        Thread.sleep(1000);
+        testAut.infect(0,1);
+        testAut.infect(1,0);
         testAut.infect(1,1);
+        States newState = testAut.checkNeighbors(0,0);
+        board.update();
+        Thread.sleep(1000);
+        testAut.getArray()[0][0].cellState = newState;
         board.update();
 
     }
